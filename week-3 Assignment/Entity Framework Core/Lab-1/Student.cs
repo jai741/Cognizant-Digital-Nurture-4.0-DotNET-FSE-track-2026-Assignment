824a3399721c29e7003699e99a4499b3c23816d8
Jaiganesh <133210394+jai741@users.noreply.github.com>
2025-07-05 21:59:45 +0530

    Delete week-3 Assignment/Entity Framework Core/Lab-1/Student.cs

--- week-3 Assignment/Entity Framework Core/Lab-1/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EFCore8Assignment.Models
-{
-    public class Student
-    {
-        public int Id { get; set; }
-        public required string Name { get; set; }
-        public required string Department { get; set; }
-    }
-}
