0273845c38664b5a766328c624726adc6b4057b0
Jaiganesh <133210394+jai741@users.noreply.github.com>
2025-07-05 21:30:23 +0530

    Add files via upload

---
+++ week-3 Assignment/Entity Framework Core/Lab-1/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EFCore8Assignment.Models;
+
+namespace EFCore8Assignment.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Replace with your SQL Server connection string
+            optionsBuilder.UseSqlServer("Server=JAI;Database=EFCore8DB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ week-3 Assignment/Entity Framework Core/Lab-1/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCore8Assignment.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Department { get; set; }
+    }
+}
